f0d88234c7e90535cfa63ee023cb66d0eea3feae
Shaik AbdulJaleel <157191067+shaikabduljaleel@users.noreply.github.com>
2024-05-14 11:07:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express=require('express')
 const app=express()
 app.get("/",(request,response)=>{
     const date=new Date();
-    response.send(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
+    response.send(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`);
 });
 module.exprots=app;
